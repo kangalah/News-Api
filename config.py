ded938d380f0291dc4bce84399d02fe7dc3c5ceb
kangalah <nyongesajoannanjala@gmail.com>
2020-07-07 19:54:29 +0300

    add the news sources url

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     NEWS_SOURCES_BASE_URL ='https://newsapi.org/v2/sources?language=en&apiKey=267d7fe5843144d9b8f75ea889ea51f7'
-    ARTICLES_BASE_URL = 'https://newsapi.org/v2/everything?domains=techcrunch.com,thenextweb.com&apiKey=267d7fe5843144d9b8f75ea889ea51f7'
+    ARTICLES_BASE_URL = 'https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=267d7fe5843144d9b8f75ea889ea51f7'
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
 
     @staticmethod
